ee4252f2eddf229cc5ba5d6632787eb72a1bfb19
learsi8918-hub <learsi8918@gmail.com>
2026-09-03 00:18:20 -0300

    Update MainActivity.java

--- app/src/main/java/com/almirante/canaril/MainActivity.java
+++ app/src/main/java/com/almirante/canaril/MainActivity.java
@@ -2,25 +2,82 @@ package com.almirante.canaril;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.webkit.ValueCallback;
+import android.content.Intent;
+import android.net.Uri;
 
 public class MainActivity extends Activity {
 
     private WebView web;
+    private ValueCallback<Uri[]> filePathCallback;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         web = new WebView(this);
+
         web.getSettings().setJavaScriptEnabled(true);
         web.getSettings().setDomStorageEnabled(true);
+
         web.setWebViewClient(new WebViewClient());
+
+        web.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public boolean onShowFileChooser(
+                    WebView webView,
+                    ValueCallback<Uri[]> callback,
+                    FileChooserParams fileChooserParams) {
+
+                filePathCallback = callback;
+
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                intent.setType("image/*");
+
+                startActivityForResult(
+                        Intent.createChooser(intent, "Escolher foto"),
+                        100
+                );
+
+                return true;
+            }
+        });
 
         web.loadUrl("file:///android_asset/index.html");
 
         setContentView(web);
+    }
+
+    @Override
+    protected void onActivityResult(
+            int requestCode,
+            int resultCode,
+            Intent data) {
+
+        if (requestCode == 100) {
+
+            if (filePathCallback != null) {
+
+                Uri[] results = null;
+
+                if (resultCode == RESULT_OK && data != null) {
+                    Uri uri = data.getData();
+
+                    if (uri != null) {
+                        results = new Uri[]{uri};
+                    }
+                }
+
+                filePathCallback.onReceiveValue(results);
+                filePathCallback = null;
+            }
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
     @Override
